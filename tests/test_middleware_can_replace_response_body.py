2840b0ec37d4c729be3448b44f49cacd6852ed1d
ellieayla <1447600+me@users.noreply.github.com>
2026-03-28 23:37:05 -0400

    Coverage: verify Wayback middleware loader

--- tests/test_middleware_can_replace_response_body.py
+++ tests/test_middleware_can_replace_response_body.py
@@ -1,8 +1,12 @@
 from datetime import timedelta
 
+import pytest
+from scrapy.crawler import Crawler
+from scrapy.exceptions import NotConfigured
 from scrapy.http import HtmlResponse, Request
 
 from event_calendars.middleware import Wayback
+from event_calendars.spiders.httpbin import HttpBinSpider
 
 
 def test_replace_body_links() -> None:
@@ -27,3 +31,21 @@ def test_replace_body_links() -> None:
 
     assert second_response.url == "http://original.one.example.com/document"
     assert response.css(".calendar-list a::attr(href)").get() == desired_url
+
+
+def test_load_wayback_middleware_from_crawler() -> None:
+    crawler = Crawler(
+        spidercls=HttpBinSpider,
+        settings={"WAYBACK_DOMAINS": ["localhost"]},
+    )
+    Wayback.from_crawler(crawler=crawler)
+
+
+def test_load_wayback_middleware_without_wayback_domains() -> None:
+    crawler = Crawler(
+        spidercls=HttpBinSpider,
+        settings={"WAYBACK_DOMAINS": []},
+    )
+
+    with pytest.raises(NotConfigured):
+        Wayback.from_crawler(crawler=crawler)
